ec0ee55caf50703af0f46b1143cb7c4987c909f5
denish rana <denishrana09@gmail.com>
2018-12-26 11:52:50 +0530

    feat: styles using lifting state up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import FormHeader from './Components/FormHeader';
 import FormContent from './Components/FormContent';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { activeClass:'g'}; //g-general, l-local, c-contact
+    this.handleActiveClassChange = this.handleActiveClassChange.bind(this);
+  }
+
+  handleActiveClassChange(activeClass){
+      this.setState({activeClass : activeClass});
+  }
+
   render() {
     return (
       <div className="container">
-        <FormHeader/>
-        <FormContent/>
+        <FormHeader
+          activeClass={this.state.activeClass}
+          onActiveClassChange={this.handleActiveClassChange}
+        />
+        <FormContent
+          activeClass={this.state.activeClass}
+        />
       </div>
     );
   }
--- src/Components/FormHeader.js
+++ src/Components/FormHeader.js
@@ -2,29 +2,61 @@ import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 class FormHeader extends Component {
-  render() {
-    return (
-      <div className="FormHeader">
-        <Grid>
-            <Row>
-                <Col className="Form-Header-Col" xs={4}>
-                    <div className="Form-Header-Text">General</div>
-                    <i className="fas fa-circle"></i>
-                    <div class="line"></div>
-                </Col>
-                <Col className="Form-Header-Col" xs={4}>
-                    <div className="Form-Header-Text">Local</div>
-                    <i className="fas fa-circle"></i>
-                    <div class="line"></div>
-                </Col>
-                <Col className="Form-Header-Col" xs={4}>
-                    <div className="Form-Header-Text">Contact</div>
-                    <i className="fas fa-circle"></i>
-                </Col>
-            </Row>
-        </Grid>
-      </div>
-    );
+    constructor(props){
+        super(props);
+        this.handleActiveClassChange = this.handleActiveClassChange.bind(this);
+    }
+
+    handleActiveClassChange(e){
+        var clickedLink = e.currentTarget;
+        var data = clickedLink.getAttribute('data-value');
+        this.props.onActiveClassChange(data);
+    }
+
+    render() {
+        //g, l, c
+        let FormHeaderClassName = "Form-Header-Col";
+        let Active = " active";
+        let class1="";
+		let class2="";
+		let class3="";
+		if(this.props.activeClass==='g'){
+			class1 = FormHeaderClassName.concat(Active);
+			class2 = FormHeaderClassName;
+            class3 = FormHeaderClassName;
+		}
+		if(this.props.activeClass==='l'){
+			class1 = FormHeaderClassName;
+			class2 = FormHeaderClassName.concat(Active);
+			class3 = FormHeaderClassName;
+		}
+		if(this.props.activeClass==='c'){
+			class1 = FormHeaderClassName;
+			class2 = FormHeaderClassName;
+			class3 = FormHeaderClassName.concat(Active);
+		}
+        return (
+        <div className="FormHeader">
+            <Grid>
+                <Row>
+                    <Col className={class1} xs={4}>
+                        <div className="Form-Header-Text">General</div>
+                        <a href="#" data-value="g" onClick={this.handleActiveClassChange}><i className="fas fa-circle"></i></a>
+                        <div className="line"></div>
+                    </Col>
+                    <Col className={class2} xs={4}>
+                        <div className="Form-Header-Text">Local</div>
+                        <a href="#" data-value="l" onClick={this.handleActiveClassChange}><i className="fas fa-circle"></i></a>
+                        <div className="line"></div>
+                    </Col>
+                    <Col className={class3} xs={4}>
+                        <div className="Form-Header-Text">Contact</div>
+                        <a href="#" data-value="c" onClick={this.handleActiveClassChange}><i className="fas fa-circle"></i></a>
+                    </Col>
+                </Row>
+            </Grid>
+        </div>
+        );
   }
 }
 
