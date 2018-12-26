751f1610b2e88680efb1960c8c8016d9bc1fdbae
denish rana <denishrana09@gmail.com>
2018-12-26 18:59:51 +0530

    fix: build url index.html

---
+++ docs/precache-manifest.bec2624d3d1786a647c9bc63c9f3d512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afc6f93e1f314557163d38fda7787888",
+    "url": "./index.html/static/media/bg.afc6f93e.jpg"
+  },
+  {
+    "revision": "0e12988117b5374df2b2",
+    "url": "./index.html/static/js/runtime~main.0e129881.js"
+  },
+  {
+    "revision": "153731d63a22ba4ba9ca",
+    "url": "./index.html/static/js/main.153731d6.chunk.js"
+  },
+  {
+    "revision": "a2eff62d58502d4a4c8b",
+    "url": "./index.html/static/js/1.a2eff62d.chunk.js"
+  },
+  {
+    "revision": "153731d63a22ba4ba9ca",
+    "url": "./index.html/static/css/main.f0c6ad1c.chunk.css"
+  },
+  {
+    "revision": "f7e77a9ad3f9fe2829010cbad310a7a5",
+    "url": "./index.html/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6e007ffc6343cd28ad9ba6372ab2c9e8.js"
+  "./index.html/precache-manifest.bec2624d3d1786a647c9bc63c9f3d512.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("./index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
